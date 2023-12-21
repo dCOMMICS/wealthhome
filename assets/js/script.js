cef4d24dbc3c7d98ba761efa34d3364ac777d5dd
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-21 23:35:40 +0300

    vertical gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,7 @@ const kNearestNeighbors = (data, labels, point, k = 3) => {
         return acc;
       },
 
-      
+
       {
         classCounts: {},
         topClass: kNearest[0].label,
@@ -51,3 +51,37 @@ const kNearestNeighbors = (data, labels, point, k = 3) => {
   
   kNearestNeighbors(data, labels, [1, 2], 2); // 1
   kNearestNeighbors(data, labels, [1, 0], 2); // 0
+
+
+  //  
+
+  const slideGallery = document.querySelector('.slides');
+const slides = slideGallery.querySelectorAll('div');
+const scrollbarThumb = document.querySelector('.thumb');
+const slideCount = slides.length;
+const slideHeight = 720;
+const marginTop = 16;
+
+const scrollThumb = () => {
+  const index = Math.floor(slideGallery.scrollTop / slideHeight);
+  scrollbarThumb.style.height = `${((index + 1) / slideCount) * slideHeight}px`;
+};
+
+const scrollToElement = el => {
+  const index = parseInt(el.dataset.id, 10);
+  slideGallery.scrollTo(0, index * slideHeight + marginTop);
+};
+
+document.querySelector('.thumbnails').innerHTML += [...slides]
+  .map(
+    (slide, i) => `<img src="${slide.querySelector('img').src}" data-id="${i}">`
+  )
+  .join('');
+
+document.querySelectorAll('.thumbnails img').forEach(el => {
+  el.addEventListener('click', () => scrollToElement(el));
+});
+
+slideGallery.addEventListener('scroll', e => scrollThumb());
+
+scrollThumb();
